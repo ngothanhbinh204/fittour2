794b1f542530ab30ab4dccf3222afc6e1e56e245
binhntps30045 <binhntps30045@fpt.edu.vn>
2024-04-01 03:30:07 +0700

    upload new ASM2

--- js/style.js
+++ js/style.js
@@ -33,4 +33,32 @@ if (window.matchMedia("(min-width:576px)").matches) {
 // slide2
 
 
+// var app = angular.module("myapp", ["ngRoute"]);
+// app.config(function ($routeProvider) {
+//     $routeProvider
+//         .when("/", { templateUrl: "index.html" })
+//         .when("/lichtrinhtour", { templateUrl: "lichtrinhtour.html" })
+//         .when("/gallery", { templateUrl: "gallery.html" })
+//         .when("/blog", { templateUrl: "blog.html" })
+//         .when("/detail", { templateUrl: "detail.html" })
+//         .when("/contact", { templateUrl: "contact.html" })
+//         .when("/tour", { templateUrl: "tour.html" })
+//         .when("/dangnhap", { templateUrl: "dangnhap.html" })
+//         .otherwise({ templateUrl: "index.html" })
+// });
+
+// app.run(function ($rootScope) {
+//     $rootScope.$on('$routeChangeStart', function () {
+//         $rootScope.loading = true;
+//     });
+//     $rootScope.$on('$routeChangeSuccess', function () {
+//         $rootScope.loading = false;
+//     });
+//     $rootScope.$on('$routeChangeError', function () {
+//         $rootScope.loading = false;
+//         alert("Lỗi, không tải được template !!!");
+//     });
+// });
+
+
 
